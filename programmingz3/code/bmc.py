3e84e69e6edcddefcb7b45a5fb217cbd2060e191
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-10-01 09:49:05 -0700

    bmc
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ programmingz3/code/bmc.py
@@ -0,0 +1,32 @@
+from z3 import *
+index = 0
+def fresh(s):
+    global index
+    index += 1
+    return Const("!f%d" % index, s)
+
+def zipp(xs, ys):
+    return [p for p in zip(xs, ys)]
+
+def bmc(init, trans, goal, fvs, xs, xns):
+    s = Solver()
+    s.add(init)
+    count = 0
+    while True:
+        print("iteration ", count)
+        count += 1
+        p = fresh(BoolSort())
+        s.add(Implies(p, goal))
+        if sat == s.check(p):
+            print (s.model())
+            return
+        s.add(trans)
+        ys = [fresh(x.sort()) for x in xs]
+        nfvs = [fresh(x.sort()) for x in fvs]
+        trans = substitute(trans, 
+                           zipp(xns + xs + fvs, ys + xns + nfvs))
+        goal = substitute(goal, zipp(xs, xns))
+        xs, xns, fvs = xns, ys, nfvs
+
+x0, x1 = Consts('x0 x1', BitVecSort(4))
+bmc(x0 == 0, x1 == x0 + 1, x0 == 10, [], [x0], [x1])
